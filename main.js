cb91fe7140fb1175c684a7ef97db569c0c3b1c8a
Am1d1v <dimadiman1adima@gmail.com>
2024-03-30 12:25:25 +0300

    Basic Canvas Setup

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+// Player Class. Movement and animation of the player character.
+class Player {
+
+}
+
+// Projectile Class. Player's shooting elements
+class Projectile {
+
+}
+
+// Enemy Class. Draw and animate enemies
+class Enemy {
+
+}
+
+// Game Class. Main logic of the game
+class Game {
+
+}
+
+
+window.addEventListener('load', () => {
+    const canvas = document.querySelector('#canvas1');
+    const context = canvas.getContext('2d');
+    canvas.width = 600;
+    canvas.height = 900;
+});
