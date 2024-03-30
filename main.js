c0fe4dee84bff2d25cdca286b866fcaf7ce3e267
Am1d1v <dimadiman1adima@gmail.com>
2024-03-30 12:59:35 +0300

    Player's temporary model added with draw and update methods

--- main.js
+++ main.js
@@ -2,7 +2,28 @@
 
 // Player Class. Movement and animation of the player character.
 class Player {
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height = 100;
+        this.x = this.game.width * 0.5 - this.width * 0.5;
+        this.y = this.game.height - this.height;
 
+        // Player's Speed
+        this.speed = 5;
+    }
+
+    // Draw Player's Model
+    draw(context){
+        context.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    // Update's Player's Data
+    update(){
+
+        // Update horizontal player position
+        this.x += this.speed;
+    }
 }
 
 // Projectile Class. Player's shooting elements
@@ -17,7 +38,18 @@ class Enemy {
 
 // Game Class. Main logic of the game
 class Game {
+    constructor(canvas){
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.player = new Player(this);
+    }
 
+    // Draw objects
+    render(context){
+        this.player.draw(context);
+        this.player.update();
+    }
 }
 
 
@@ -26,4 +58,16 @@ window.addEventListener('load', () => {
     const context = canvas.getContext('2d');
     canvas.width = 600;
     canvas.height = 900;
+
+    const game = new Game(canvas);
+    
+
+    function animate(){
+        // Clear Canvas
+        context.clearRect(0, 0, this.width, this.height);
+
+        game.render(context);
+        window.requestAnimationFrame(animate);
+    }
+    animate();
 });
