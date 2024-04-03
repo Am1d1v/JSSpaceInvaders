64a39373a8c3ea54969f87b984aa942f50a7f86b
Am1d1v <dimadiman1adima@gmail.com>
2024-04-03 15:55:34 +0300

    Handle sprite frames

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 class Player {
     constructor(game){
         this.game = game;
-        this.width = 100;
-        this.height = 100;
+        this.width = 140;
+        this.height = 120;
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = this.game.height - this.height;
 
@@ -14,11 +14,25 @@ class Player {
 
         // Player's Speed
         this.speed = 10;
+
+        this.image = document.querySelector('#player');
+
+        // Horizontal Frames
+        this.frameX = 0;
     }
 
     // Draw Player's Model
     draw(context){
-        context.fillRect(this.x, this.y, this.width, this.height)
+        //context.fillRect(this.x, this.y, this.width, this.height);
+
+        // Handle sprite frames
+        if(this.game.keys.indexOf('1') > -1){
+            this.frameX = 1;
+        } else {
+            this.frameX = 0;
+        }
+
+        context.drawImage(this.image, this.frameX * this. width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 
     // Update's Player's Data
