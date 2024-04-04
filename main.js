5a5778f3767359d0671abd6449c4085b87b4d794
Am1d1v <dimadiman1adima@gmail.com>
2024-04-04 14:33:15 +0300

    Boss status added. Destroyed Boss sprite frames added

--- main.js
+++ main.js
@@ -298,18 +298,25 @@ class Boss {
     draw(context){
         context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
 
-        context.save();
-        context.textAlign = 'center';
-        context.shadowOffsetX = 3;
-        context.shadowOffsetY = 3;
-        context.shadowColor = 'black';
-        context.fillText(this.lives, this.x + this.width * 0.5, this.y + 50)
-        context.restore();
+        // Show health only if health points > 1;
+        if(this.lives > 0){
+            context.save();
+            context.textAlign = 'center';
+            context.shadowOffsetX = 3;
+            context.shadowOffsetY = 3;
+            context.shadowColor = 'black';
+            context.fillText(this.lives, this.x + this.width * 0.5, this.y + 50)
+            context.restore();
+        }
+
+        
     }
 
     update(){
         this.speedY = 0;
         if(this.y < 0) this.y += 10;
+
+        if(this.game.spriteUpdate && this.lives > 0) this.frameX = 0;
 
         // Boss Boundaries
         if(this.x < 0 || this.x > this.game.width - this.width){
@@ -323,16 +330,22 @@ class Boss {
         this.game.projectilesPool.forEach(projectile => {
             
             if(this.game.checkCollision(this, projectile) && !projectile.free && this.lives > 0){
-                this.lives--;
+                this.hit(1);
                 projectile.reset();
             };
-        });
+        }); 
+
+        // Boss Destroyed
+        if(this.lives < 1 && this.game.spriteUpdate){
+            this.frameX++;
+        }
+
     }
 
     // Change frameX when enemy gets hit. 
     hit(damage){
         this.lives -= damage;
-        this.frameX = this.maxLives - Math.floor(this.lives);
+        if(this.lives > 0) this.frameX = 1;
     }
 }
 
